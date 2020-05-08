3f84f2e6ea6a9799eb4b34146e0f5ba5b9eae361
Patrick <patrick204nqh@gmail.com>
2020-05-09 00:40:33 +0700

    add deleteTransaction

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,10 +18,19 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  // Action
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
       }}
     >
       {children}
